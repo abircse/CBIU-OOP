669c18b44fec1c7c52a1f521fe9b4f5880524d0b
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2019-09-16 20:27:08 +0600

    initial Comment

---
+++ src/cbiu/ac/bd/HelloWorld.java
@@ -0,0 +1,25 @@
+package cbiu.ac.bd;
+
+// this is a class
+public class HelloWorld {
+
+    // this is a main method
+    public static void main(String[] args) {
+
+       // Define variable
+        int a = 10;
+        int b = 20;
+        int sum;
+
+        // addition operation
+        sum = a+b;
+
+        // printing result of sum operation
+        System.out.println(sum);
+
+
+
+
+
+    }
+}
