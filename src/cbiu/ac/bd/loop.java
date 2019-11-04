30a09dd7fb9ca199a82b6c8bb8ffd6695f8d9c6a
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2019-11-04 16:18:12 +0600

    initial Comment

---
+++ src/cbiu/ac/bd/loop.java
@@ -0,0 +1,19 @@
+package cbiu.ac.bd;
+
+import java.util.Scanner;
+
+public class loop {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+
+        int result = a+b;
+
+
+
+    }
+}
